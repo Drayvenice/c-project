817e97b3378ef93a9254989b4fc90ff4591dc037
tksthii <Nnguyen0011@conestogac.on.ca>
2026-04-13 22:20:21 -0400

    implement NNClassifier - euclideanDistance, train, predict, evaluate

--- NNClassifier.cpp
+++ NNClassifier.cpp
@@ -5,28 +5,59 @@
 /**
  * @file NNClassifier.cpp
  * @brief Implementation of the NN classifier.
- * @author [Your Name] & [Teammate Name]
+ * @author Kristen & Drayven
  */
 
 double NNClassifier::euclideanDistance(const DataPoint& a, const DataPoint& b) const {
     // TODO: return sqrt( (a.x-b.x)^2 + (a.y-b.y)^2 + (a.z-b.z)^2 )
-    return 0.0;
+    return sqrt(
+        pow(a.x - b.x, 2) +
+        pow(a.y - b.y, 2) +
+        pow(a.z - b.z, 2)
+    );
 }
 
 void NNClassifier::train(const std::vector<DataPoint>& trainingData) {
     // TODO: Store trainingData into trainingData_
+    trainingData_ = trainingData;
+    std::cout << "[NNClassifier] Training complete. "
+        << trainingData_.size() << " samples loaded." << std::endl;
 }
 
 int NNClassifier::predict(const DataPoint& point) {
     // TODO: Loop through trainingData_
     // TODO: Find the point with smallest euclideanDistance
     // TODO: Return that point's label
-    return -1;
+    double minDist = DBL_MAX;
+    int bestLabel = -1;
+
+    for (const auto& trainPoint : trainingData_) {
+        double d = euclideanDistance(point, trainPoint);
+        if (d < minDist) {
+            minDist = d;
+            bestLabel = trainPoint.label;
+        }
+    }
+
+    return bestLabel;
 }
 
 double NNClassifier::evaluate(const std::vector<DataPoint>& testData) {
     // TODO: For each point in testData, call predict()
     // TODO: Compare predicted label vs actual label
     // TODO: Return accuracy = (correct / total) * 100
-    return 0.0;
+    int correct = 0;
+
+    for (const auto& point : testData) {
+        int predicted = predict(point);
+        if (predicted == point.label) {
+            correct++;
+        }
+    }
+
+    double accuracy = (double)correct / testData.size() * 100.0;
+    std::cout << "[NNClassifier] Accuracy: " << correct << "/"
+        << testData.size() << " = " << accuracy << "%" << std::endl;
+
+    return accuracy;
 }
